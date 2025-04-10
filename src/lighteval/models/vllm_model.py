4c6ef1469c8ab20a2b7ed3fa89a01f5185e64a16
Iacob Alexandru-Andrei <iacobalexandruandrei@gmail.com>
2025-04-10 13:08:29 +0100

    Fix VLLM

--- src/lighteval/models/vllm_model.py
+++ src/lighteval/models/vllm_model.py
@@ -98,8 +98,9 @@ class VLLMModel(LightevalModel):
 
     def cleanup(self):
         destroy_model_parallel()
-        if self.model is not None:
-            del self.model.llm_engine.model_executor.driver_worker
+        # NOTE: This breaks newer VLLM versions.
+        # if self.model is not None:
+        #     del self.model.llm_engine.model_executor.driver_worker
         self.model = None
         gc.collect()
         ray.shutdown()
